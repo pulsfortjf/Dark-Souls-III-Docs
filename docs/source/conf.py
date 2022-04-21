5633bc70f4e39caa2d4f3faf46d16c9f555b348a
pulsfortjf <89221045+pulsfortjf@users.noreply.github.com>
2022-04-21 12:23:55 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,7 +29,31 @@ templates_path = ['_templates']
 
 # -- Options for HTML output
 
+import sphinx_rtd_theme
+
 html_theme = 'sphinx_rtd_theme'
+
+# Theme options are theme-specific and customize the look and feel of a theme
+# further.  For a list of options available for each theme, see the
+# documentation.
+#
+# html_theme_options = {}
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
+
+# Custom sidebar templates, must be a dictionary that maps document names
+# to template names.
+#
+# The default sidebars (for documents that don't match any pattern) are
+# defined by theme itself.  Builtin themes are using these templates by
+# default: ``['localtoc.html', 'relations.html', 'sourcelink.html',
+# 'searchbox.html']``.
+#
+# html_sidebars = {}
+
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
